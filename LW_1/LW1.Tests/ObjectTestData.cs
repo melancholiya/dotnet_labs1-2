9d923269ce9c25f4e0726c1fe291f87f3b41b0d9
Yanok27 <gavrilchikyana27@gmail.com>
2023-10-09 16:31:39 +0300

    added the ability to work with an empty collection

--- LW_1/LW1.Tests/ObjectTestData.cs
+++ LW_1/LW1.Tests/ObjectTestData.cs
@@ -1,14 +1,24 @@
+using System.Collections.Generic;
+
 namespace LW1.Tests
 {
     public class ObjectTestData
     {
-        public static object[] TestCases =
+        public static object[] TestCasesWithValues =
         {
             new object[] { 42,56,56 },
             new object[] { "Alice", "Bob", "Bob" },
             new object[] { true, false, false },
             new object[] { new MyClassA { Value = 42 }, new MyClassA { Value = 56 }, new MyClassA { Value = 56 } },
             new object[] { new MyClassB { Name = "Alice" }, new MyClassB { Name = "Bob" }, new MyClassB { Name = "Bob" } },
+        };
+        public static object[] TestCasesWithEmptyCollections =
+        {
+            new object[] { new List<int>(), 42 },
+            new object[] { new List<string>(), "Alice" },
+            new object[] { new List<bool>(), true },
+            new object[] { new List<MyClassA>(), new MyClassA { Value = 42 } },
+            new object[] { new List<MyClassB>(), new MyClassB { Name = "Alice" } },
         };
 
         private class MyClassA
